371e1860c84eb94779711efbd047c5104e1e7c76
Roma <romansipyak@gmail.com>
2018-09-21 15:47:17 +0300

    delete timestamps from cities and categories

---
+++ db/migrate/20180921124619_remove_timestamp_from_categories.rb
@@ -0,0 +1,6 @@
+class RemoveTimestampFromCategories < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :categories, :created_at, :datetime
+    remove_column :categories, :updated_at, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_09_115739) do
+ActiveRecord::Schema.define(version: 2018_09_21_124619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,14 +27,10 @@ ActiveRecord::Schema.define(version: 2018_09_09_115739) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "cities", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
